163f7d3636812741b8af24c792d254b373881957
Paul ADENOT <paul@paul.cx>
2011-11-20 17:24:54 +0100

    New `Go to top` thingy, with new icon and quadratic progression.
    Removed log

---
+++ static/js/static.js
@@ -0,0 +1,13 @@
+function top() {
+  var page_size = window.innerHeight + window.scrollMaxY;
+  var accel = 1.1;
+  function up() {
+    // Quadratic progression
+    accel *= accel;
+    window.scrollBy(0,-accel);
+    if (window.pageYOffset !== 0) {
+      setTimeout(up, 33);
+    }
+  }
+  setTimeout(up, 33);
+}

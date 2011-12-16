8c586fd88e1b0c74d0040bff42b855ef3f7b2363
Paul ADENOT <paul@paul.cx>
2011-12-16 12:09:13 +0100

    Fixed scroll to top button. Added title rewriting.

--- static/js/static.js
+++ static/js/static.js
@@ -1,39 +1,66 @@
-function top() {
-  var page_size = window.innerHeight + window.scrollMaxY;
-  var accel = 1.1;
-  function up() {
-    console.log(accel);
-    if (window.pageYOffset < page_size / 10) {
-      if (accel > 10) {
-        accel *= 0.50;
+function static_js() {
+  function top() {
+    var page_size = window.innerHeight + window.scrollMaxY;
+    var accel = 1.1;
+    function up() {
+      console.log(accel);
+      if (window.pageYOffset < page_size / 10) {
+        if (accel > 10) {
+          accel *= 0.50;
+        } else {
+          accel *= 0.99;
+        }
       } else {
-        accel *= 0.99;
+        accel *= accel;
       }
-    } else {
-      accel *= accel;
-    }
-    if (accel >= page_size/20) {
-      accel = page_size/20;
-    }
+      if (accel >= page_size/20) {
+        accel = page_size/20;
+      }
 
-    window.scrollBy(0,-accel);
+      window.scrollBy(0,-accel);
 
-    if (window.pageYOffset >= 0.01) {
-      setTimeout(up, 33);
+      if (window.pageYOffset >= 0.01) {
+        setTimeout(up, 33);
+      }
     }
+    setTimeout(up, 33);
   }
-  setTimeout(up, 33);
-}
 
-function init_scroll() {
-  var totoc =  document.getElementById("totoc");
-  if (totoc) {
-    totoc.style.opacity = 0;
-  }
-  window.addEventListener("scroll", function() {
+  function init_scroll() {
     var totoc =  document.getElementById("totoc");
-    if(totoc) {
-      totoc.style.opacity = window.pageYOffset / window.scrollMaxY;
+    if (totoc) {
+      totoc.style.opacity = 0;
     }
-  }, false);
+    window.addEventListener("scroll", function() {
+      var totoc =  document.getElementById("totoc");
+      if(totoc) {
+        totoc.style.opacity = window.pageYOffset / window.scrollMaxY;
+      }
+    }, false);
+    var t = document.querySelector("div#totoc");
+    if (t) {
+      t.addEventListener("click", top, false);
+    }
+  }
+
+  function get_subdomain() {
+    var re = /http:\/\/([\-a-zA-Z0-9]+)[.]isbullsh[.]it/;
+    var loc = re.exec(window.location);
+    if (loc) {
+      if (loc[0] != "isbullsh") {
+        return loc[0];
+      }
+    }
+    return "";
+  }
+
+  function replace_header() {
+    var sub = get_subdomain();
+    if (sub.length) {
+      document.querySelector("h1 > a > span").innerHTML = sub;
+    }
+  }
+
+  replace_header();
+  init_scroll();
 }
